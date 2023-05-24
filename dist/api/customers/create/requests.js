2d27aa9981108a3a51d95a1eeb8eb0907de14663
Segun <oniludesegun@gmail.com>
2023-05-24 14:53:26 +0100

    customer-create

--- dist/api/customers/create/requests.js
+++ dist/api/customers/create/requests.js
@@ -1,11 +1,12 @@
-const bash = `curl https://api.paystack.co/customer
--H "Authorization: Bearer YOUR_SECRET_KEY"
--H "Content-Type: application/json"
+const sh = `#!/bin/sh
+curl https://api.paystack.co/customer \
+-H "Authorization: Bearer YOUR_SECRET_KEY" \
+-H "Content-Type: application/json" \
 -d '{ "email": "zero@sum.com",
       "first_name": "Zero",
       "last_name": "Sum",
       "phone": "+2348123456789"
-    }'
+    }' \
 -X POST`
 
 const js = `const https = require('https')
@@ -77,4 +78,4 @@ const php = `<?php
   echo $result;
 ?>`
 
-export { bash, js, php }
+export {sh, js, php}
