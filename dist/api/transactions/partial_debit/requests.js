fa497d921def3ce1feba15d9e3d6ac4e915dfc9e
Damilola Odujoko <damilola.odujoko@paystack.com>
2023-05-25 12:41:22 +0100

    Port transaction code snippets

--- dist/api/transactions/partial_debit/requests.js
+++ dist/api/transactions/partial_debit/requests.js
@@ -1,13 +1,15 @@
-const bash = `curl https://api.paystack.co/transaction/partial_debit
--H "Authorization: Bearer YOUR_SECRET_KEY"
--H "Content-Type: application/json"
--d '{ 
-      "authorization_code": "AUTH_72btv547", 
-      "currency": "NGN", 
-      "amount": "20000",
-      "email": "customer@email.com"
- }'
--X POST`
+const sh = `#!/bin/sh
+url="https://api.paystack.co/transaction/partial_debit"
+authorization="Authorization: Bearer YOUR_SECRET_KEY"
+content_type="Content-Type: application/json"
+data='{ 
+  "authorization_code": "AUTH_72btv547", 
+  "currency": "NGN", 
+  "amount": "20000",
+  "email": "customer@email.com"
+}'
+
+curl "$url" -H "$authorization" -H "$content_type" -d "$data" -X POST`
 
 const js = `const https = require('https')
 
@@ -78,4 +80,4 @@ const php = `<?php
   echo $result;
 ?>`
 
-export { bash, js, php }
+export {sh, js, php}
