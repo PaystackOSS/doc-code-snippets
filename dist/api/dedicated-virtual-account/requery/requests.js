7ebca0fd19b5e56624a67cb910ddaeb711b81134
Damilola Odujoko <damilola.odujoko@paystack.com>
2023-05-26 11:05:13 +0100

    Update DVA requery shell

--- dist/api/dedicated-virtual-account/requery/requests.js
+++ dist/api/dedicated-virtual-account/requery/requests.js
@@ -3,12 +3,11 @@ accountNumber="1234567890"
 providerSlug="example-provider"
 date="2023-05-30"
 
-url="https://api.paystack.co/dedicated_account/requery?account_number=${accountNumber}&provider_slug=${providerSlug}&date=${date}"
+url="https://api.paystack.co/dedicated_account/requery?account_number=$accountNumber&provider_slug=$providerSlug&date=$date"
 authorization="Authorization: Bearer YOUR_SECRET_KEY"
 content_type="Content-Type: application/json"
 
-curl "$url" -H "$authorization" -H "$content_type" -X GET
-`
+curl "$url" -H "$authorization" -H "$content_type" -X GET`
 
 const js = `var https = require('follow-redirects').https;
 var fs = require('fs');
