a66a2ef2ec0ce5a78b7eefffcee747290a503137
Segun <oniludesegun@gmail.com>
2023-05-31 22:18:49 +0100

    api-payment-requests

---
+++ api/payment-requests/total/index.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+url="https://api.paystack.co/paymentrequest/totals"
+authorization="Authorization: Bearer YOUR_SECRET_KEY"
+
+curl "$url" -H "$authorization"  -X GET
+
+
+
+
+
+
+
---
+++ api/payment-requests/verify/index.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+url="https://api.paystack.co/paymentrequest/verify/:code"
+authorization="Authorization: Bearer YOUR_SECRET_KEY"
+
+curl "$url" -H "$authorization" -X GET
+
+
+
+
+
+
+
+
+
