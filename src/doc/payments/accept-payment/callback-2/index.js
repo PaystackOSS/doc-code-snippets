39a57b0db1e9f57384114ad78adaa46c1bfd6c63
Damilola Odujoko <damilola.odujoko@paystack.com>
2023-06-26 12:39:44 +0100

    Remove old code

--- src/doc/payments/accept-payment/callback-2/index.js
+++
@@ -1,4 +0,0 @@
-callback: function(response) {
-  window.location = "http://www.yoururl.com/verify_transaction.php?reference=" + response.reference;
-};
-// On the redirected page, you can call Paystack's verify endpoint.
