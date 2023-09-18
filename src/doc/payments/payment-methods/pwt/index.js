7a5e4aa6e2798988e4d0768edf9c65942f52c29e
Damilola Odujoko <damilola.odujoko@paystack.com>
2023-09-18 13:05:21 +0100

    Feat: Add PwT on Charge API code snippets

---
+++ src/doc/payments/payment-methods/pwt/index.js
@@ -0,0 +1,37 @@
+const https = require('https')
+
+const params = JSON.stringify({ 
+  "email": "another@one.com", 
+  "amount": "25000",
+  "bank_transfer": {
+    "account_expires_at": "2023-09-12T13:10:00Z"
+  }
+})
+
+const options = {
+  hostname: 'api.paystack.co',
+  port: 443,
+  path: '/charge',
+  method: 'POST',
+  headers: {
+    Authorization: 'Bearer SECRET_KEY',
+    'Content-Type': 'application/json'
+  }
+}
+
+const req = https.request(options, res => {
+  let data = ''
+
+  res.on('data', (chunk) => {
+    data += chunk
+  });
+
+  res.on('end', () => {
+    console.log(JSON.parse(data))
+  })
+}).on('error', error => {
+  console.error(error)
+})
+
+req.write(params)
+req.end()
