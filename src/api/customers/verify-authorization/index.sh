ba8c3c60ab851daa1da1c81e027ed292ebde3de6
Damilola Odujoko <damilola.odujoko@paystack.com>
2023-11-01 09:37:27 +0100

    Feat: Direct Debit update

--- src/api/customers/verify-authorization/index.sh
+++ src/api/customers/verify-authorization/index.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-url="https://api.paystack.co/transaction/verify/{reference}"
+url="https://api.paystack.co/customer/authorization/verify/{reference}"
 authorization="Authorization: Bearer YOUR_SECRET_KEY"
 
 curl "$url" -H "$authorization" -X GET
