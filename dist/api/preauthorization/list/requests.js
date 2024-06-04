10c7d02b9b8b1c9600557d1404f01b3fe1a81bee
Andrew Nduati <andrew@paystack.com>
2024-06-04 18:10:13 +0300

    add php code snippet for list preauthorizations

--- dist/api/preauthorization/list/requests.js
+++ dist/api/preauthorization/list/requests.js
@@ -31,4 +31,33 @@ const req = https.request(options, res => {
 })
 req.end()`
 
-export {sh, js}
+const php = `<?php
+  $curl = curl_init();
+  
+  curl_setopt_array($curl, array(
+    CURLOPT_URL => "https://api.paystack.co/preauthorization",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "GET",
+    CURLOPT_HTTPHEADER => array(
+      "Authorization: Bearer SECRET_KEY",
+      "Cache-Control: no-cache",
+    ),
+  ));
+  
+  $response = curl_exec($curl);
+  $err = curl_error($curl);
+
+  curl_close($curl);
+  
+  if ($err) {
+    echo "cURL Error #:" . $err;
+  } else {
+    echo $response;
+  }
+?>`
+
+export {sh, js, php}
