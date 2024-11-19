3558d7c4457f9c411cf008a3efd6decb1b5f13b7
Andrew Nduati <andrew@paystack.com>
2024-11-19 14:38:25 +0300

    copy fix and update

--- src/api/terminal/update/index.sh
+++ src/api/terminal/update/index.sh
@@ -3,7 +3,7 @@ url="https://api.paystack.co/terminal/{terminal_id}"
 authorization="Authorization: Bearer YOUR_SECRET_KEY"
 content_type="Content-Type: application/json"
 data='{ 
-  "address": "Somewhere on earth
+  "address": "Somewhere on earth"
 }'
 
 curl "$url" -H "$authorization" -H "$content_type" -d "$data" -X PUT
--- src/api/transfers/initiate/index.sh
+++ src/api/transfers/initiate/index.sh
@@ -5,7 +5,8 @@ content_type="Content-Type: application/json"
 data='{ 
   "source": "balance", 
   "reason": "Calm down", 
-  "amount":3794800, "recipient": "RCP_gx2wn530m0i3w3m"
+  "amount":3794800, 
+  "recipient": "RCP_gx2wn530m0i3w3m"
 }'
 
 curl "$url" -H "$authorization" -H "$content_type" -d "$data" -X POST
