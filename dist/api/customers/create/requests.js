ab037732324b5aba5d5d50c77c95fcbe8f1a493e
Andrew Nduati <andrew@paystack.com>
2024-11-19 14:38:25 +0300

    copy fix and update

--- dist/api/customers/create/requests.js
+++ dist/api/customers/create/requests.js
@@ -3,7 +3,7 @@ url="https://api.paystack.co/customer"
 authorization="Authorization: Bearer YOUR_SECRET_KEY"
 content_type="Content-Type: application/json"
 data='{ 
-    "email": "zero@sum.com",
+    "email": "customer@example.com",
     "first_name": "Zero",
     "last_name": "Sum",
     "phone": "+2348123456789"
