420140fe61c1f52b3be40561344c9eb39ff96881
tolu-paystack <tolu@paystack.com>
2025-02-10 20:45:13 +0100

    update create vt code snippet

--- src/doc/terminal/virtual-terminal/create-virtual-terminal/index.sh
+++ src/doc/terminal/virtual-terminal/create-virtual-terminal/index.sh
@@ -1,13 +1,10 @@
-#!/bin/sh
-
-url="https://api.paystack.co/virtual_terminal"
-authorization="Authorization: Bearer YOUR_SECRET_KEY"
-content_type="Content-Type: application/json"
-data='{
+curl "https://api.paystack.co/virtual_terminal" \
+-H "Authorization: Bearer YOUR_SECRET_KEY" \
+-H "Content-Type: application/json" \
+-d '{
   "name": "Sales Point #1",
   "destinations": [
     {"target": "+2347081234567"}
   ]
-}'
-
-curl "$url" -H "$authorization" -H "$content_type" -d "$data" -X POST
+}' \
+-X POST
