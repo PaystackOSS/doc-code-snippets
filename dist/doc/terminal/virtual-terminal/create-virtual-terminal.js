1711b62a5264783e7c4d3a5f507c6add4b7fc5b2
tolu-paystack <tolu@paystack.com>
2025-02-11 10:26:31 +0100

    build changes

--- dist/doc/terminal/virtual-terminal/create-virtual-terminal.js
+++ dist/doc/terminal/virtual-terminal/create-virtual-terminal.js
@@ -69,15 +69,15 @@ const php = `<?php
 ?>
 `
 
-const sh = `curl "https://api.paystack.co/virtual_terminal" \
--H "Authorization: Bearer YOUR_SECRET_KEY" \
--H "Content-Type: application/json" \
+const sh = `curl "https://api.paystack.co/virtual_terminal"
+-H "Authorization: Bearer YOUR_SECRET_KEY"
+-H "Content-Type: application/json"
 -d '{
   "name": "Sales Point #1",
   "destinations": [
     {"target": "+2347081234567"}
   ]
-}' \
+}'
 -X POST`
 
 const json = `{
