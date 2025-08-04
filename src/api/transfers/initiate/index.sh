e0ea5fe2b6b2fe660d674db12b07ab04b0d0f03e
Damilola Odujoko <damilola.odujoko@paystack.com>
2025-08-04 12:42:00 +0100

    chore: whitespace clean up

--- src/api/transfers/initiate/index.sh
+++ src/api/transfers/initiate/index.sh
@@ -4,10 +4,10 @@ authorization="Authorization: Bearer YOUR_SECRET_KEY"
 content_type="Content-Type: application/json"
 data='{ 
   "source": "balance",
-	"reason": "Bonus for the week",
-	"amount": 100000,
-	"recipient": "RCP_gd9vgag7n5lr5ix",
-  "reference": "ACV_9ee55786-2323-4760-98e2-6380c9cb3f68"
+  "amount": 100000,
+  "recipient": "RCP_gd9vgag7n5lr5ix",
+  "reference": "ACV_9ee55786-2323-4760-98e2-6380c9cb3f68",
+  "reason": "Bonus for the week"
 }'
 
 curl "$url" -H "$authorization" -H "$content_type" -d "$data" -X POST
